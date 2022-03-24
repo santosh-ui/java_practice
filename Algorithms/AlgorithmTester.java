11c36546b7563b4368f9f8429dcd9c2df14bc2a1
Sai Santosh Raju Prathigadapa <sp21acg@herts.ac.uk>
2022-03-24 09:40:21 +0000

    performed linear search algorithm

---
+++ Algorithms/AlgorithmTester.java
@@ -0,0 +1,7 @@
+public class AlgorithmTester {
+    public static void main(String[] args) {
+        LinearSearch l1 = new LinearSearch();
+        int [] array = {1, 5, 2, 56, 3, 9};
+        System.out.println(l1.linearSearch(array, 9));
+    }
+}
